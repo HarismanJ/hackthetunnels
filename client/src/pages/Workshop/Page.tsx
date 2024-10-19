e288e37de4dd1015283ece13ec8b15f3bbd0d291
Harisman Jeyakanthan <harismanjeyakanthan@dhcp-91-247.cu-internet.carleton.ca>
2024-10-19 12:02:08 -0400

    Problem 0 and 1 done

--- client/src/pages/Workshop/Page.tsx
+++ client/src/pages/Workshop/Page.tsx
@@ -2,25 +2,18 @@ import "./style.scss";
 import Component from "./Component";
 
 function Page() {
-  return (
-    <div>
-      <h1>Example Page</h1>
-      <p>
-        Lorem Ipsum is simply dummy text of the printing and typesetting
-        industry. Lorem Ipsum has been the industry's standard dummy text ever
-        since the 1500s, when an unknown printer took a galley of type and
-        scrambled it to make a type specimen book. It has survived not only five
-        centuries, but also the leap into electronic typesetting, remaining
-        essentially unchanged. It was popularised in the 1960s with the release
-        of Letraset sheets containing Lorem Ipsum passages, and more recently
-        with desktop publishing software like Aldus PageMaker including versions
-        of Lorem Ipsum.
-      </p>
-      <Component label="Label 1" />
-      <Component label="Label 2" />
-      <Component label="Label 3" />
-    </div>
-  );
+  return <div>
+    <h1>Workshop</h1>
+    <h2>Variables</h2>
+    <p>Workshop Content</p>
+    <button><img src= "https://i1.sndcdn.com/artworks-X86u42lPsZP8Ui1x-sf4Nbw-t500x500.jpg"></img></button>
+    <input type="text" placeholder="enter name"></input>
+    
+
+
+
+
+  </div>
 }
 
 export default Page;
